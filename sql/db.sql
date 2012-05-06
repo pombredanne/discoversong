d163851ad0eb287a01927e7bda1aae84c0a4b75e
Eugene Efremov <eugene@rd.io>
2012-05-06 10:22:13 -0700

    fix this up - created new schema discodb, put tables in there (migrated to Heroku's new free postgres add-in off of the shared beta thing)

--- sql/db.sql
+++ sql/db.sql
@@ -1,20 +1,15 @@
--- Table: discoversong_user
+DROP TABLE discodb.discoversong_user;
 
--- DROP TABLE discoversong_user;
-
-CREATE TABLE discoversong_user
+CREATE TABLE discodb.discoversong_user
 (
-  rdio_user_id integer,
   id serial NOT NULL,
+  rdio_user_id integer,
   token text,
   secret text,
   address text,
-  playlist text,
   prefs bytea,
   CONSTRAINT pk PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE discoversong_user
-  OWNER TO tguaspklkhnrpn;
